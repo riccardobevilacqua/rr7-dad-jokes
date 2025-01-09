4d6006d2a5919b6fa8e5e6dfb48a9709e1025d2f
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2025-01-09 22:27:44 +0100

    Updated loader structure

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,24 +1,10 @@
-// This import brings in TypeScript type definitions from a local types file
-// It imports the Route type which defines the expected types for:
-// - Route.LoaderArgs - Arguments passed to the loader function
-// - Route.ComponentProps - Props passed to the Home component
 import type { Route } from "./+types/home";
+
+import type { Joke } from "~/types";
+import { loader as getJokes } from "~/api/getJokes";
 import JokeCard from "~/components/JokeCard";
 
-interface Joke {
-  id: string;
-  joke: string;
-}
-
-export async function loader({}: Route.LoaderArgs) {
-  const response = await fetch("https://icanhazdadjoke.com/search?limit=5", {
-    headers: {
-      Accept: "application/json",
-    },
-  });
-  const data = await response.json();
-  return data.results;
-}
+export const loader = getJokes;
 
 export default function Home({ loaderData }: Route.ComponentProps) {
   const jokes = loaderData as Joke[];
